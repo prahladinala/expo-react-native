6fb00dad48906f401d2d560024fcbdcf5c4c4ead
prahladinala <prahlad2018@gmail.com>
2021-05-08 11:56:10 +0530

    TouchableWithoutFeedback Component

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image
-        blurRadius={10}
-        fadeDuration={3000}
-        resizeMode={'repeat'}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }} />
+      <TouchableWithoutFeedback onPress={() => { console.log("Image Pressed") }}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }} />
+      </TouchableWithoutFeedback>
       <Text>Hello React Native</Text>
     </View >
   );
