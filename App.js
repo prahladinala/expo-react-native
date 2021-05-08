927dd882ecf3ede369c55006054282d719acbf46
prahladinala <prahlad2018@gmail.com>
2021-05-08 12:11:59 +0530

    Alert Component - creating  a custom alert

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, Alert } from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button color="orange" title="Click Me" onPress={() => { alert("Button Tapped") }} />
+      <Button color="orange"
+        title="Click Me"
+        onPress={() => Alert.alert("My Title", "My Message", [{ text: "Yes", onPress: () => console.log("YES") }, { text: "No", onPress: () => console.log("NO") }])} />
     </View >
   );
 }
