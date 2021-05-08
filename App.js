a58a3073018b56f95bcd8667c8ed4b5b6fc933f7
prahladinala <prahlad2018@gmail.com>
2021-05-08 12:31:02 +0530

    Platform - Writing platform specific code

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, View, Alert } from 'react-native';
+import { Button, StyleSheet, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
 
   return (
-    <View style={[styles.container, styles.text, containerStyles]}>
+    <SafeAreaView style={styles.container}>
       <Button color="orange"
         title="Click Me"
         onPress={() => Alert.prompt("title", "Description", (text) => console.log(text))} />
-    </View >
+    </SafeAreaView >
   );
 }
 
-const containerStyles = { backgroundColor: "blue" }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 20 : 0,
   },
-  text: {
-    backgroundColor: 'green',
-  }
 });
