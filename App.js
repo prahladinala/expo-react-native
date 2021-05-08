aeb12427afcfe2e2c1db0b7d372331566e362d29
prahladinala <prahlad2018@gmail.com>
2021-05-08 11:41:40 +0530

    Image Component - Loading static images from our project folder

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  const handlePress = () => {
-    console.log("TEXT PRESSED")
-    alert("Multi Line Function")
-  }
-  return (
 
+  return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Hello React Native - Long Text to check Number of Lines numberOfLines={1}</Text>
-    </View>
+      <Image source={require('./assets/favicon.png')} />
+      <Text>Hello React Native</Text>
+    </View >
   );
 }
 
