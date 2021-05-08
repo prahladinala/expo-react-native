3ce9bebf37171b62b8d7f30da2ec5919d2d4cb9a
prahladinala <prahlad2018@gmail.com>
2021-05-08 11:57:17 +0530

    TouchableOpacity Component

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={() => { console.log("Image Pressed") }}>
+      <TouchableOpacity onPress={() => { console.log("Image Pressed") }}>
         <Image
           source={{
             width: 200,
             height: 300,
             uri: "https://picsum.photos/200/300",
           }} />
-      </TouchableWithoutFeedback>
+      </TouchableOpacity>
       <Text>Hello React Native</Text>
     </View >
   );
