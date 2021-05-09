085d4bbd93e005c6cc1fd365cdd1fcbf7faa57b4
prahladinala <prahlad2018@gmail.com>
2021-05-09 13:26:05 +0530

    React Native Flexbox - alignItems stretch

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { View } from 'react-native';
 export default function App() {
   return (
     // PARENT
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "center", alignItems: 'flex-start' }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "center", alignItems: 'stretch' }}>
       {/* CHILDREN */}
-      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }} />
+      <View style={{ backgroundColor: "dodgerblue", width: 100 }} />
       <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
