5aa5b0554330789d0a3cdda408901a37d21ed878
prahladinala <prahlad2018@gmail.com>
2021-05-09 13:03:07 +0530

    React Native Flexbox - flexDirection column - Default(No need to use this property)

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 export default function App() {
   return (
     // PARENT
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "column-reverse" }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "column" }}>
       {/* CHILDREN */}
       <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
