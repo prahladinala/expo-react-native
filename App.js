4de0a1944f3f87d24b595d187e845f75ac502bcd
prahladinala <prahlad2018@gmail.com>
2021-05-09 13:19:57 +0530

    React Native Flexbox - justifyContent space-around

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 export default function App() {
   return (
     // PARENT
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "flex-start" }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "space-around" }}>
       {/* CHILDREN */}
       <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
