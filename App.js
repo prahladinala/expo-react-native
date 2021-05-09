43f98968a3504e3875245e89e53ceebfb99435c3
prahladinala <prahlad2018@gmail.com>
2021-05-09 13:01:25 +0530

    React Native Flexbox - flexDirection Setup

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default function App() {
     // PARENT
     <View style={{ backgroundColor: "#fff", flex: 1 }}>
       {/* CHILDREN */}
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1.5 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
