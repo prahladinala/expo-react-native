4c0107773c36c93ebb681e80219863d727089aa8
prahladinala <prahlad2018@gmail.com>
2021-05-09 13:24:03 +0530

    React Native Flexbox - without  alignItems baseline

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { View } from 'react-native';
 export default function App() {
   return (
     // PARENT
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "center", alignItems: 'baseline' }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row", justifyContent: "center" }}>
       {/* CHILDREN */}
-      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
     // Justify Content => Aligns items center of main/primary axis (Horizontal axis if flexDirection is row)
