c12da801de1e5f31cb30727617d6a4e4afa6d113
prahladinala <prahlad2018@gmail.com>
2021-05-09 12:59:59 +0530

    React Native Flexbox - One Parent 3 Children 4parts

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={{ backgroundColor: "#fff", flex: 1 }}>
       {/* CHILDREN */}
       <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
+      <View style={{ backgroundColor: "gold", flex: 1.5 }} />
       <View style={{ backgroundColor: "tomato", flex: 1 }} />
     </View>
   );
