8e29a56686428086cfd25510d0232bf5c61e3c36
prahladinala <prahlad2018@gmail.com>
2021-05-11 00:11:51 +0530

    Flexbox - bottom

--- App.js
+++ App.js
@@ -21,13 +21,13 @@ export default function App() {
         backgroundColor: "gold",
         width: 100,
         height: 100,
-        top: 20
+        bottom: 20
       }} />
       <View style={{
         backgroundColor: "tomato",
         width: 100,
         height: 100,
-        top: -20
+        bottom: -20
       }} />
 
     </View>
